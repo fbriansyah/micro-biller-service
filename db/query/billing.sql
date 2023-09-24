d4cffb4d0ee49bb0d770bb69483a2e8e7afd9d6f
Febriansyah <fbriansyah@gmail.com>
2023-09-24 21:39:01 +0700

    add PayBill and CheckBill query

--- db/query/billing.sql
+++ db/query/billing.sql
@@ -7,4 +7,23 @@ VALUES (
 
 --name: GetBillingByNumber
 SELECT * FROM billings
-WHERE bill_number
+WHERE bill_number = $1 LIMIT 1;
+
+--name: PayBill :one
+UPDATE billings
+SET
+    isPayed = true,
+    pay_timestampt = now(),
+    refference_number = $1
+WHERE
+    bill_number = $2
+AND total_amount = $3
+RETURNING *;
+
+--name: CheckBill :one
+SELECT * FROM billings
+WHERE 
+    bill_number = $1 
+AND refference_number = $2
+AND total_amount = $3
+LIMIT 1;
