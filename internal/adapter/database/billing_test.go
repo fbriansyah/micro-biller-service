875bd18a3531eb701a11751917ac4242c35f8211
Febriansyah <fbriansyah@gmail.com>
2023-09-25 17:28:14 +0700

    add TestGetBillingByNumber test

--- internal/adapter/database/billing_test.go
+++ internal/adapter/database/billing_test.go
@@ -38,3 +38,19 @@ func CreateRandomBilling(t *testing.T) Billing {
 func TestCreateRandomBilling(t *testing.T) {
 	CreateRandomBilling(t)
 }
+
+func TestGetBillingByNumber(t *testing.T) {
+	bill1 := CreateRandomBilling(t)
+
+	bill2, err := testQueries.GetBillingByNumber(context.Background(), bill1.BillNumber)
+	require.NoError(t, err)
+	require.NotEmpty(t, bill2)
+
+	require.Equal(t, bill1.Name, bill2.Name)
+	require.Equal(t, bill1.BillNumber, bill2.BillNumber)
+	require.Equal(t, bill1.BaseAmount, bill2.BaseAmount)
+	require.Equal(t, bill1.FineAmount, bill2.FineAmount)
+	require.Equal(t, bill1.TotalAmount, bill2.TotalAmount)
+	require.Equal(t, bill1.IsPayed.Bool, bill2.IsPayed.Bool)
+	require.Equal(t, bill1.RefferenceNumber, bill2.RefferenceNumber)
+}
