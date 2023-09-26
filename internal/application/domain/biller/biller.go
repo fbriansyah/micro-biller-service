b5a0c631bb2b0b0f697ce1489c8c5f731dc3743c
Febriansyah <fbriansyah@gmail.com>
2023-09-26 19:34:28 +0700

    add json mapper to billing domain

--- internal/application/domain/biller/biller.go
+++ internal/application/domain/biller/biller.go
@@ -3,15 +3,15 @@ package biller
 import "time"
 
 type Bill struct {
-	BillNumber  string
-	Name        string
-	BaseAmount  int64
-	FineAmount  int64
-	TotalAmount int64
+	BillNumber  string `json:"bill_number"`
+	Name        string `json:"name"`
+	BaseAmount  int64  `json:"base_amount"`
+	FineAmount  int64  `json:"fine_amount"`
+	TotalAmount int64  `json:"total_amount"`
 }
 
 type Transaction struct {
-	Billing          Bill
-	RefferenceNumber string
-	CreatedAt        time.Time
+	Billing          Bill      `json:"bill"`
+	RefferenceNumber string    `json:"refference_number"`
+	CreatedAt        time.Time `json:"created_at"`
 }
