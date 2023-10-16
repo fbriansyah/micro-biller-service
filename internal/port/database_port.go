07986996a82e17623f5a7ea1bed0867cc50c4ac0
Febriansyah <fbriansyah@gmail.com>
2023-10-16 20:10:21 +0700

    add method comment

--- internal/port/database_port.go
+++ internal/port/database_port.go
@@ -7,8 +7,12 @@ import (
 )
 
 type DatabasePort interface {
+	// CheckBill check payment status to given bill number
 	CheckBill(ctx context.Context, arg db.CheckBillParams) (db.Billing, error)
+	// CreateBilling create new billing in table billings
 	CreateBilling(ctx context.Context, arg db.CreateBillingParams) (db.Billing, error)
+	// Find billing for given bill number
 	GetBillingByNumber(ctx context.Context, billNumber string) (db.Billing, error)
+	// PayBill set billing status to payed
 	PayBill(ctx context.Context, arg db.PayBillParams) (db.Billing, error)
 }
--- internal/port/service_port.go
+++ internal/port/service_port.go
@@ -5,7 +5,10 @@ import (
 )
 
 type BillerServicePort interface {
+	// Inquiry find billing using bill number
 	Inquiry(billNumber string) (dbill.Bill, error)
+	// Payment update bill to payed and set refference number
 	Payment(updateBill dbill.Bill, refferenceNumber string) (dbill.Transaction, error)
+	// Advice check bill is already payed or not
 	Advice(searchBill dbill.Bill, refferenceNumber string) (dbill.Transaction, error)
 }
